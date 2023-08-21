c06b34c5238fe4ae585dd0b2e5f9c3bd6cc75803
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2023-08-21 23:31:10 +0300

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Financial_Accounting.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Financial_Accounting.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Financial_Accounting.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
